2ad07c3ee89f2ca6949e4e31c188bafbf66300b4
Oscar Martinez de la O <51036348+oscarmtzo@users.noreply.github.com>
2024-11-28 04:08:42 +0000

    fix - se corrijio la compilacion correcta del archivo en C ya que solo realizaba un echo a la linea correspondiente a la compilacion con gcc

--- gcc_compile_automation_script_example.sh
+++ gcc_compile_automation_script_example.sh
@@ -11,5 +11,7 @@ echo -e "\nAutomatizacion de Compilacion de archivo \n"
 nombre_nuevo_archivo_compilado="${nombre_archivo%.*}.out"
 #echo $nombre_nuevo_archivo_compilado
 echo "gcc $nombre_archivo -o $nombre_nuevo_archivo_compilado"
+
+gcc "$nombre_archivo" -o "$nombre_nuevo_archivo_compilado"
 # Automatizacion de ejecucion de script en c
 ./"${nombre_nuevo_archivo_compilado}"
